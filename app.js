546779059e9bb6cc8ca017c38db99ab62302f782
Haider3966 <ashrafhaider143@gamil.com>
2024-11-09 23:05:48 +0600

    Add a meaningful commit message

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import fileUpload from "express-fileupload";
 import userRouter from './routes/userRouter.js'
 import applicationRouter from './routes/applicationRouter.js'
 import jobRouter from './routes/jobRouter.js'
+import {dbConnection} from './database/dbConnection.js'
 
 const app = express();
 dotenv.config({ path: "./config/config.env" });
@@ -32,6 +33,8 @@ app.use(
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
-//dbConnection();
+
+
+dbConnection();
 
 export default app;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import app from "./app.js";
+
 import cloudinary from "cloudinary";
 
 cloudinary.v2.config({
